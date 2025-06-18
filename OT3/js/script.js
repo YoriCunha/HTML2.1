7ce108e38021d0f52e809ee35cea6291d8a8f9a6
YoriCunha <yori_cunha@estudante.sc.senai.br>
2025-06-17 21:29:25 -0300

    Ajustes

--- OT3/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('menu-link').addEventListener('click', function(e) {
-    e.preventDefault();
-    var submenu = document.getElementById('submenu');
-    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-});
